5481b9ed447584d37d3288445a8fb832edd88582
Ravi <33181670+raysk4ever@users.noreply.github.com>
2024-09-03 23:19:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
   const { pokemons } = usePokemon();
   const { playersArr, players } = usePlayers();
 
+  // testing
   return (
     <main className="App">
       <Header />
